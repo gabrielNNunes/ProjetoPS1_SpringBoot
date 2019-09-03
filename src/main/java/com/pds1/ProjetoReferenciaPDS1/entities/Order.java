425dfe5013acb7905c8b30874521eac1bc416db2
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-09-03 20:12:40 -0300

    Subtotal & Total methods

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/entities/Order.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/entities/Order.java
@@ -43,6 +43,7 @@ public class Order implements Serializable {
 	@OneToOne(mappedBy="order", cascade = CascadeType.ALL)
 	private Payment payment;	
 	
+		
 	public Order() {
 		super();
 	}
@@ -101,6 +102,13 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+	public Double getTotal(){
+		double sum = 0.0;
+		for(OrderItem x : items){
+			sum += x.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
