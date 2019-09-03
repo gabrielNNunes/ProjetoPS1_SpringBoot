a30977437c1d10c330598a9e7a8b22e125b6f2fd
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-09-03 19:42:31 -0300

    Payment, one-to-one association

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/config/TestConfig.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pds1.ProjetoReferenciaPDS1.entities.Category;
 import com.pds1.ProjetoReferenciaPDS1.entities.Order;
 import com.pds1.ProjetoReferenciaPDS1.entities.OrderItem;
+import com.pds1.ProjetoReferenciaPDS1.entities.Payment;
 import com.pds1.ProjetoReferenciaPDS1.entities.Product;
 import com.pds1.ProjetoReferenciaPDS1.entities.User;
 import com.pds1.ProjetoReferenciaPDS1.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
