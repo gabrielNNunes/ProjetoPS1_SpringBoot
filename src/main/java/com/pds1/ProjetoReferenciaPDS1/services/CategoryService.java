4ab92905fc8109ad83d5acaba94903d7922fbfe6
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-09-10 21:57:22 -0300

    Category - insert, delete, update

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/services/CategoryService.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/services/CategoryService.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.pds1.ProjetoReferenciaPDS1.entities.Category;
 import com.pds1.ProjetoReferenciaPDS1.repositories.CategoryRepository;
+
+import services.exceptions.DatabaseException;
+import services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,5 +26,28 @@ public class CategoryService {
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	public void delete(Long id){
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	public Category update(Long id,Category obj) {
+		Category entity = repository.getOne(id);
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
+		
+		
 	}
 }
