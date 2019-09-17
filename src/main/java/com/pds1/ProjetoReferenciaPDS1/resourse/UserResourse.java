90fce9b5f6f427e707bbdcaf9f2cf0c7f0bc0a0e
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-09-17 20:25:45 -0300

    User insert

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/resourse/UserResourse.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/resourse/UserResourse.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pds1.ProjetoReferenciaPDS1.dto.UserDTO;
+import com.pds1.ProjetoReferenciaPDS1.dto.UserInsertDTO;
 import com.pds1.ProjetoReferenciaPDS1.entities.User;
 import com.pds1.ProjetoReferenciaPDS1.services.UserService;
 
@@ -38,10 +39,10 @@ public class UserResourse {
 	}
 	
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj){
-		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj);
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto){
+		UserDTO newDTO = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDTO);
 	}
 	
 	@DeleteMapping(value = "/{id}")
