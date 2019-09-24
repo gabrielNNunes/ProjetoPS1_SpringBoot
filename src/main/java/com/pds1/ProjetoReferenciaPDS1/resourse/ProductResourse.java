928f6dc9c07c9d8840bf505e0bbe0c6d3e245f81
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-09-24 20:57:13 -0300

    Product update

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/resourse/ProductResourse.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/resourse/ProductResourse.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,12 @@ public class ProductResourse {
 		ProductDTO newDTO = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDTO.getId()).toUri();
 		return ResponseEntity.created(uri).body(newDTO);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id,@RequestBody ProductCategoriesDTO dto){
+		ProductDTO newDTO = service.update(id, dto);
+		return ResponseEntity.ok().body(newDTO);
 	}
 	
 }
