0d042c28c173b2e6236456cd4bbed655a76fe5b3
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-09-27 19:24:39 -0300

    Custom user insert validation: unique email

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/dto/UserInsertDTO.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/dto/UserInsertDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.pds1.ProjetoReferenciaPDS1.entities.User;
+import com.pds1.ProjetoReferenciaPDS1.services.validation.UserInsertValid;
 
+@UserInsertValid
 public class UserInsertDTO implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
