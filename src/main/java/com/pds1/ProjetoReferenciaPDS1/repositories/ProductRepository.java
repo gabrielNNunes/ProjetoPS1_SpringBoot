3dfb13e8955f25a03d9c87d8f185d2cc43b3f2e7
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-10-15 19:42:42 -0300

    Find products by category

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/repositories/ProductRepository.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/repositories/ProductRepository.java
@@ -1,9 +1,18 @@
 package com.pds1.ProjetoReferenciaPDS1.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.pds1.ProjetoReferenciaPDS1.entities.Category;
 import com.pds1.ProjetoReferenciaPDS1.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
-
+		
+	@Transactional(readOnly = true)
+	@Query("SELECT obj FROM Product	obj INNER JOIN obj.categories cats WHERE :category IN cats")
+	Page<Product> findByCategory(Category category, Pageable pageable);
+	
 }
