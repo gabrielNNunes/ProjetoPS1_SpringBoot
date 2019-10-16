a51ca0152d8ece5e1c9771c6bf3f2ef322d58cdc
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-10-15 21:51:28 -0300

    Product detailed search

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/repositories/ProductRepository.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.pds1.ProjetoReferenciaPDS1.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +12,14 @@ import com.pds1.ProjetoReferenciaPDS1.entities.Category;
 import com.pds1.ProjetoReferenciaPDS1.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) AND cats IN :categories")
+	Page<Product> findByNameContainingIgnoreCaseAndCategoriesIn(String name,List<Category> categories, Pageable pageable);
+		
+	@Transactional(readOnly = true)
+	@Query("SELECT obj FROM Product obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+	Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
 		
 	@Transactional(readOnly = true)
 	@Query("SELECT obj FROM Product	obj INNER JOIN obj.categories cats WHERE :category IN cats")
--- src/main/java/com/pds1/ProjetoReferenciaPDS1/resourses/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/resourses/exceptions/ResourceExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import services.exceptions.DatabaseException;
 import services.exceptions.JWTAuthenticationException;
 import services.exceptions.JWTAuthorizationException;
+import services.exceptions.ParamFormatException;
 import services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -58,6 +59,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> jWTAuthorization(JWTAuthorizationException e, HttpServletRequest request){
 		String error = "Authentication error";
 		HttpStatus status = HttpStatus.FORBIDDEN;
+		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(ParamFormatException.class)
+	public ResponseEntity<StandardError> paramFormat(ParamFormatException e, HttpServletRequest request){
+		String error = "Format error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
