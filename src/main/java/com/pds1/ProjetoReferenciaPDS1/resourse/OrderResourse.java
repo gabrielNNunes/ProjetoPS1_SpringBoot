dc664758ff311936b37230a112dcf4e05e0564f0
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-10-18 19:24:37 -0300

    Update Order

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/resourse/OrderResourse.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/resourse/OrderResourse.java
@@ -9,11 +9,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.pds1.ProjetoReferenciaPDS1.dto.CategoryDTO;
 import com.pds1.ProjetoReferenciaPDS1.dto.OrderDTO;
 import com.pds1.ProjetoReferenciaPDS1.dto.OrderItemDTO;
 import com.pds1.ProjetoReferenciaPDS1.services.OrderService;
@@ -61,6 +63,13 @@ public class OrderResourse {
 		OrderDTO orderDTO = service.placeOrder(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(orderDTO.getId()).toUri();
 		return ResponseEntity.created(uri).body(orderDTO);
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<OrderDTO> update(@PathVariable Long id,@RequestBody OrderDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 }
