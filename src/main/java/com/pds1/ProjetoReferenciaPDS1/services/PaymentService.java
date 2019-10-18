3ed9609041529e3d87ac6caf7b71d7471f6dda30
Gabriel N Nunes <gabrieln.nunes@yahoo.com.br>
2019-10-18 20:06:26 -0300

    Payment PUT POST endpoints

--- src/main/java/com/pds1/ProjetoReferenciaPDS1/services/PaymentService.java
+++ src/main/java/com/pds1/ProjetoReferenciaPDS1/services/PaymentService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pds1.ProjetoReferenciaPDS1.dto.PaymentDTO;
+import com.pds1.ProjetoReferenciaPDS1.entities.Order;
 import com.pds1.ProjetoReferenciaPDS1.entities.Payment;
+import com.pds1.ProjetoReferenciaPDS1.repositories.OrderRepository;
 import com.pds1.ProjetoReferenciaPDS1.repositories.PaymentRepository;
 
 import services.exceptions.ResourceNotFoundException;
@@ -19,6 +24,9 @@ public class PaymentService {
 	@Autowired
 	private PaymentRepository repository;
 	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	public List<PaymentDTO> findAll(){
 		List<Payment> list = repository.findAll();
 		return list.stream().map(e -> new PaymentDTO(e)).collect(Collectors.toList());
@@ -28,5 +36,29 @@ public class PaymentService {
 		Payment entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new PaymentDTO(entity);
 	}
-
+	
+	@Transactional
+	public PaymentDTO insert(PaymentDTO dto) {
+		Order order = orderRepository.getOne(dto.getOrderId());
+		Payment payment = new Payment(null, dto.getMoment(),order);
+		order.setPayment(payment);
+		orderRepository.save(order);
+		return new PaymentDTO(order.getPayment());
+	}
+	
+	@Transactional 
+	public PaymentDTO update(Long id,PaymentDTO dto) {
+		try {
+		Payment entity = repository.getOne(id);
+		updateData(entity,dto);
+		entity = repository.save(entity);
+		return new PaymentDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	private void updateData(Payment entity, PaymentDTO dto) {
+		entity.setMoment(dto.getMoment());		
+		
+	}
 }
